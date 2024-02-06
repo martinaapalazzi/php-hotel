018d1e22b1e1f7c3d266d0e546f9988c67f9f792
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-06 15:07:32 +0100

    trial tables

--- index.php
+++ index.php
@@ -43,6 +43,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PHP Hotel</title>
 </head>
 <body>
@@ -54,7 +55,74 @@
                     echo '<li>'.$hotel['name'].', '.$hotel['description'].', '.$hotel['parking'].', '.$hotel['vote'].', '.$hotel['distance_to_center'].'</li>';
                 }
             ?>
+
+            <?php     
+                //for ($i = 0; $i <= count($hotels); $i++){
+                //    echo '<li>'.$i['name'].'</li>';
+                //}
+            ?> 
         </ul>   
+    </div>
+
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+
+                <?php
+                    //echo '<tr>'.'<th scope="col">'.array_keys($hotels).'</th>'.'</tr>'
+                ?>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <?php
+                        foreach ($hotels as $hotel) {
+                            echo '<td>'.$hotel['name'].'</td>';
+                        }
+                    ?>
+                </tr>
+
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    <td>@mdo</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
     
 </body>
